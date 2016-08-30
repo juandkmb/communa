665c810e0a01f2f1afcbfadf81906c92ebd23899
Juan <juan@donkeymob.com>
2016-08-30 13:29:05 -0400

    ray munich requested changes

--- ready/js/mighty.js
+++ ready/js/mighty.js
@@ -6,4 +6,19 @@ $(document).ready (function(){
                $("#success-alert").alert('close');
                 });
             });
- });
+
+  var type_text = $('#type_text');
+  type_text.text(type_text.attr('data-tw-a'));
+  // sync with css animation
+  var switch_text_timer = window.setInterval( switch_text, type_text.attr('data-tw-time') );
+  var toggler = 0;
+  function switch_text(){
+  	if(toggler === 0){
+  		type_text.text(type_text.attr('data-tw-b'));
+  		toggler = 1;
+  	}else{
+  		type_text.text(type_text.attr('data-tw-a'));
+  		toggler = 0;
+  	}
+  }
+});
